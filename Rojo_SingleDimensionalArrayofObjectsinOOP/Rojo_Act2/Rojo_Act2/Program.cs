f062e81168a6ff10e334f066c3240ae1513bf434
rojokazzy <rojokazzy9@gmail.com>
2026-01-21 08:13:16 +0800

    Add files via upload

---
+++ Rojo_SingleDimensionalArrayofObjectsinOOP/Rojo_Act2/Rojo_Act2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rojo_Act2
+{
+    internal class Program
+    {
+        //Rojo Kazzy Rhys Z.
+        //IT401A
+        //Single Dimensional Array of Objects in OOP
+        static void Main(string[] args) //Main program
+        {
+            //Create an array of TitleofMovies objects with a size of 6
+            TitleofMovies[] movies = new TitleofMovies[6];
+
+            //Add Protagonist objects to the array
+            movies[0] = new Protagonist("Harry Potter", "Fantasy", "Harry Potter");
+            movies[1] = new Protagonist("Marrowbone", "Horror/Mystery", "Jack Marrowbone");
+            movies[2] = new Protagonist("The Spiderwick Chronicles", "Family/Adventure", "Jared Grace");
+            //Add Director objects to the array
+            movies[3] = new Director("Enola Holmes", "Mystery/Adventure", "Harry Bradbeer");
+            movies[4] = new Director("Ready Player One", "Sci-fi/Action", "Steven Spielberg");
+            movies[5] = new Director("Interstellar", "Sci-fi/Adventure", "Christopher Nolan");
+
+            //Loop through each movie object in the array
+            foreach (TitleofMovies titleofMovies in movies)
+            {
+                titleofMovies.Info();
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ Rojo_SingleDimensionalArrayofObjectsinOOP/Rojo_Act2/Rojo_Act2/TitleofMovies.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rojo_Act2
+{
+    class TitleofMovies //Base class
+    {
+        //Stores the movie info
+        public string title;
+        public string genre;
+
+        //Constructor that initializes the title and genre
+        public TitleofMovies(string ktitle, string kgenre)
+        {
+            title = ktitle;
+            genre = kgenre;
+        }
+        
+        public virtual void Info() //Constructor that initializes the title and genre
+        {
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Genre: " + genre);
+        }
+    }
+    class Protagonist : TitleofMovies //Derived class
+    {
+        //Stores the protagonist's character name
+        public string characterName;
+
+        //Constructor that passes title and genre to the base class
+        public Protagonist(string ktitle, string kgenre, string kcharacterName) : base(ktitle, kgenre)
+        {
+            characterName = kcharacterName;
+        }
+        //Overrides the Info method to include protagonist details
+        public override void Info()
+        {
+            Console.WriteLine("\n--Protagonist--");
+            base.Info(); //Calls the base class Info method
+            Console.WriteLine("Character Name: " + characterName);
+        }
+    }
+    class Director : TitleofMovies //Derived class
+    {
+        //Stores the director's character name
+        public string name;
+
+        //Constructor that passes title and genre to the base class
+        public Director(string ktitle, string kgenre, string kname) : base(ktitle, kgenre)
+        {
+            name = kname;
+        }
+        //Overrides the Info method to include director details
+        public override void Info()
+        {
+            Console.WriteLine("\n--Director--");
+            base.Info(); //Calls the base class Info method
+            Console.WriteLine("Name: " + name);
+        }
+    }
+}
